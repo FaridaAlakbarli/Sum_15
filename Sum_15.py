1c1f42316c979e2308a847fe5374c1af9b9e7ea2
FaridaKitty <85605770+FaridaKitty@users.noreply.github.com>
2022-11-12 12:31:46 -0800

    Update Sum_15.py

--- Sum_15.py
+++ Sum_15.py
@@ -1,32 +1,34 @@
-full = list(map(int, input('Enter numbers: ').split()))
-summ = sum(full)
+def change_to_15(lis):
+    summ = sum(lis)
+    if summ > 15 :
+        dif = summ - 15
+        sorted_list = sorted(lis, reverse = True)
+        for i in sorted_list:
+            if i > dif:
+                lis[lis.index(i)] = i - dif
+                dif = 0
+            elif i == dif:
+                lis[lis.index(i)] = i - (dif-1)
+                dif = 1
+            elif i < dif:
+                x = (dif - i) + 1
+                lis[lis.index(i)] = i - (dif - x)
+                dif = x
+    elif summ < 15:
+        dif = 15 - summ
+        sorted_list = sorted(lis)
+        for i in sorted_list:
+            if 9 - i < dif:
+                x = (dif - 9) + 1
+                lis[lis.index(i)] = i + (dif - x)
+                dif = x
+            else:
+                lis[lis.index(i)] = i + dif
+                dif = 0
+    else:
+        print('The sum is 15, no need to change! :)')
+    
+    print(lis)            
 
-if summ > 15 :
-    dif = summ - 15
-    sorted_full = sorted(full, reverse = True)
-    for i in sorted_full:
-        if i > dif:
-            full[full.index(i)] = i - dif
-            dif = 0
-        elif i == dif:
-            full[full.index(i)] = i - (dif-1)
-            dif = 1
-        elif i < dif:
-            x = (dif - i) + 1
-            full[full.index(i)] = i - (dif - x)
-            dif = x
-elif summ < 15:
-    dif = 15 - summ
-    sorted_full = sorted(full)
-    for i in sorted_full:
-        if 9 - i < dif:
-            x = (dif - 9) + 1
-            full[full.index(i)] = i + (dif - x)
-            dif = x
-        else:
-            full[full.index(i)] = i + dif
-            dif = 0
-else:
-    print('The sum is 15, no need to change! :)')
-            
-print(full)
+n = list(map(int, input('Enter 3 numbers: ').split()))
+change_to_15(n)
